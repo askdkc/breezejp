bafba6494072c9b29f77995c11a81908565ce2dc
askdkc <askdkc@users.noreply.github.com>
2022-11-03 07:49:55 +0900

    Update CommandTest.php

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -9,6 +9,12 @@ it('runs setup command without error', closure: function () {
 });
 
 it('can see published files', closure: function () {
+    $this->artisan('breezejp')
+        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
+        ->expectsConfirmation('GitHubリポジトリにスターの御協力をお願いします🙏', 'no')
+        ->expectsOutput('日本語ファイルのインストールが完了しました!')
+        ->assertExitCode(0);
+    
     $this->assertFileExists(base_path('lang/ja.json'));
     $this->assertFileExists(base_path('lang/ja/auth.php'));
     $this->assertFileExists(base_path('lang/ja/pagination.php'));
