25f5949687c6520e28aebbeee30ceef68aa50782
dkc <askdkc@users.noreply.github.com>
2022-11-16 10:36:30 +0900

    Add file cleanup before test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,6 +15,25 @@ class TestCase extends Orchestra
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Askdkc\\Breezejp\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
+
+        // コマンドが出力したファイルがテスト前に残っていたら消す
+        if(is_dir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja')) {
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja.json');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja/auth.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja/pagination.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja/passwords.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja/validation.php');
+            rmdir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/lang/ja');
+        }
+
+        if(is_dir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile')) {
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile/edit.blade.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile/partials/delete-user-form.blade.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile/partials/update-password-form.blade.php');
+            unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile/partials/update-profile-information-form.blade.php');
+            rmdir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile/partials');
+            rmdir(__DIR__.'/../vendor/orchestra/testbench-core/laravel/resources/views/profile');
+        }
     }
 
     protected function getPackageProviders($app)
