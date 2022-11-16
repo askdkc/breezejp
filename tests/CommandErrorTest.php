a2026cf857afd326e2ce96ad3c8aedaf9471f58d
dkc <askdkc@users.noreply.github.com>
2022-11-16 10:36:06 +0900

    delete file

--- tests/CommandErrorTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-it('setup command failed with an error', closure: function () {
-    $this->artisan('breezejp')
-        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
-        ->expectsOutput('Laravel BreezeのProfile用に翻訳可能なbladeを準備します')
-        ->assertExitCode(1);
-});
