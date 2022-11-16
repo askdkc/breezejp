8afe45c34351e55f6340b2cf6a2ff68d9f50d647
dkc <askdkc@users.noreply.github.com>
2022-11-16 10:36:34 +0900

    Update CommandTest.php

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -1,30 +1,31 @@
 <?php
-//
-//use Illuminate\Http\Testing\File;
-//
-//it('runs setup command without error', closure: function () {
-//    File::fake()->create(resource_path('views/profile/') . 'dummy.php', 10);
-//
-//    $this->artisan('breezejp')
-//        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
-//        ->expectsOutput('Laravel BreezeのProfile用に翻訳可能なbladeを準備します')
-//        ->expectsConfirmation('GitHubリポジトリにスターの御協力をお願いします🙏', 'no')
-//        ->expectsOutput('日本語ファイルのインストールが完了しました!')
-//        ->assertExitCode(0);
-//});
-//
-//it('can see published files', closure: function () {
-//    File::fake()->create(resource_path('views/profile/') . 'dummy.php', 10);
-//
-//    $this->artisan('breezejp')
-//        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
-//        ->expectsConfirmation('GitHubリポジトリにスターの御協力をお願いします🙏', 'no')
-//        ->expectsOutput('日本語ファイルのインストールが完了しました!')
-//        ->assertExitCode(0);
-//
-//    $this->assertFileExists(base_path('lang/ja.json'));
-//    $this->assertFileExists(base_path('lang/ja/auth.php'));
-//    $this->assertFileExists(base_path('lang/ja/pagination.php'));
-//    $this->assertFileExists(base_path('lang/ja/passwords.php'));
-//    $this->assertFileExists(base_path('lang/ja/validation.php'));
-//});
+
+test('breezejp command stop successfully because Breeze is not installed', closure: function () {
+
+    $this->artisan('breezejp')
+        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
+        ->expectsOutput('Laravel BreezeのProfile用に翻訳可能なbladeを準備します')
+        ->expectsOutput('先にLaravel Breezeをインストールしてください')
+        ->expectsConfirmation('強制的に実行しますか？(Breezeを使わずバリデーションの日本語化利用時等はyesを選択)', 'no')
+        ->assertExitCode(1);
+});
+
+test('breezejp command forcefully run and see all the published files', closure: function () {
+    $this->artisan('breezejp')
+        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
+        ->expectsOutput('Laravel BreezeのProfile用に翻訳可能なbladeを準備します')
+        ->expectsOutput('先にLaravel Breezeをインストールしてください')
+        ->expectsConfirmation('強制的に実行しますか？(Breezeを使わずバリデーションの日本語化利用時等はyesを選択)', 'yes')
+        ->expectsOutput('実行完了')
+        ->expectsConfirmation('GitHubリポジトリにスターの御協力をお願いします🙏', 'no')
+        ->expectsOutput('日本語ファイルのインストールが完了しました!')
+        ->assertExitCode(0);
+
+    $this->assertFileExists(base_path('lang/ja.json'));
+    $this->assertFileExists(base_path('lang/ja/auth.php'));
+    $this->assertFileExists(base_path('lang/ja/pagination.php'));
+    $this->assertFileExists(base_path('lang/ja/passwords.php'));
+    $this->assertFileExists(base_path('lang/ja/validation.php'));
+});
+
+
