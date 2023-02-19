751162c5833327bb63dc9d3ae5b67227a42675da
dkc <askdkc@users.noreply.github.com>
2023-02-19 16:10:00 +0900

    Add test for Timezone

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -23,4 +23,17 @@ test('breezejp command successfully update config/app.php locale to ja', functio
 
     $configfile = file_get_contents(base_path('config/app.php'));
     $this->assertStringContainsString("'locale' => 'ja'", $configfile);
+    $this->assertStringContainsString("'faker_locale' => 'ja_JP'", $configfile);
+    $this->assertStringContainsString("'timezone' => 'Asia/Tokyo'", $configfile);
+});
+
+test('breezejp command successfully update config/app.php timezone to Asia/Tokyo', function () {
+    $this->artisan('breezejp')
+        ->expectsOutput('Laravel Breeze用に日本語翻訳ファイルを準備します')
+        ->expectsConfirmation('GitHubリポジトリにスターの御協力をお願いします🙏', 'no')
+        ->expectsOutput('日本語ファイルのインストールが完了しました!')
+        ->assertExitCode(0);
+
+    $configfile = file_get_contents(base_path('config/app.php'));
+    $this->assertStringContainsString("'timezone' => 'Asia/Tokyo'", $configfile);
 });
