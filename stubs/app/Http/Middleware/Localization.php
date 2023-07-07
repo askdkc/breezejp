bd8e2cb6f3e9ffc914ec90901849ac9f6cce9785
dkc <askdkc@users.noreply.github.com>
2023-07-08 07:51:03 +0900

    Add Language Switcher Resource Files

---
+++ stubs/app/Http/Middleware/Localization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class Localization
+{
+    /**
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @return mixed
+    */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        }
+        return $next($request);
+    }
+}
