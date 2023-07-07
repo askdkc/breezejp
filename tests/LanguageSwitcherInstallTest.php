179af986a65e8822d23c34f5559e8c5037ae8ffd
dkc <askdkc@users.noreply.github.com>
2023-07-08 08:45:15 +0900

    Add Language Switcher Install Command Test

---
+++ tests/LanguageSwitcherInstallTest.php
@@ -0,0 +1,14 @@
+<?php
+
+test('breezejp language switcher command successfully', closure: function () {
+    $this->artisan('breezejp --langswitch')
+        ->expectsOutput('言語切替用のRoute language/{locale} を準備します')
+        ->expectsOutput('言語切替用の Middleware を準備します')
+        ->expectsOutput('Kernel に Middleware を登録します')
+        ->expectsOutput('Language Switherのインストールが完了しました!')
+        ->assertExitCode(0);
+
+    $this->assertFileExists(base_path('app/Http/Middleware/Localization.php'));
+    $webfile = file_get_contents(base_path('routes/web.php'));
+    $this->assertStringContainsString("// Language Switcher Route 言語切替用ルートだよ", $webfile);
+});
