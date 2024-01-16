2a3f8b0070e5c19d64e3ce19b1b20b22dcf0b817
askdkc <askdkc@users.noreply.github.com>
2024-01-16 23:49:03 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,7 @@ class TestCase extends Orchestra
     {
         parent::setUp();
 
-        copy (__DIR__.'/../vendor/orchestra/testbench-core/laravel/.env.example', __DIR__.'/../vendor/orchestra/testbench-core/laravel/.env');
+        copy(__DIR__.'/../vendor/orchestra/testbench-core/laravel/.env.example', __DIR__.'/../vendor/orchestra/testbench-core/laravel/.env');
 
         // テスト用のファイルが残ってたら消す(web.php)
         if (is_file(__DIR__.'/../vendor/orchestra/testbench-core/laravel/routes/web.php')) {
