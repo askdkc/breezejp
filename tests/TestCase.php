71aebd2ea90ed95ded3f3a2556a2848c808ef8c4
askdkc <askdkc@users.noreply.github.com>
2025-02-25 23:52:52 +0900

    update test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,17 +19,6 @@ class TestCase extends Orchestra
             unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/config/app.php');
         }
 
-        // テスト用のファイル作成(app.php)
-        if (strtok(app()->version(), '.') >= 12) {
-            if (! is_file(__DIR__.'/../vendor/orchestra/testbench-core/laravel/config/app.php')) {
-                copy(__DIR__.'/laravel12/app.php', __DIR__.'/../vendor/orchestra/testbench-core/laravel/config/app.php');
-            }
-        } else {
-            if (! is_file(__DIR__.'/../vendor/orchestra/testbench-core/laravel/config/app.php')) {
-                copy(__DIR__.'/laravel11/app.php', __DIR__.'/../vendor/orchestra/testbench-core/laravel/config/app.php');
-            }
-        }
-
         // テスト用のファイルが残ってたら消す(web.php)
         if (is_file(__DIR__.'/../vendor/orchestra/testbench-core/laravel/routes/web.php')) {
             unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/routes/web.php');
